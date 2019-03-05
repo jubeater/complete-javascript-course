dc0230a32157eed6917c384952ee3eb43b5da33c
jubeater <zhangtianlin15@gmail.com>
2019-03-05 00:06:51 -0600

    basic datatypes in js

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,43 @@
+var firstName = 'John';
+console.log(firstName);
+//str
+var lastName = 'Smith';
+
+//number
+var age =  28;
+
+//bool
+var fullAge = true;
+console.log(fullAge);
+
+//undefined
+var job;
+console.log(job);
+
+//except char we can only use _ or $ as the var name
+
+//this is the single line comment
+
+/*
+this is multi line comments
+this is multi line comments
+*/
+
+
+/*
+var firstName = 'John';
+var age = 28;
+Type coercion make the type of age change from number to str
+*/
+console.log(firstName + ' ' + age);
+
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + 
+'. Is he married? ' + isMarried);
+
+
+//Variable mutation
