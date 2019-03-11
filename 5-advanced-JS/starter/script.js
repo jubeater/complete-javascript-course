7dfef177e68d0ffc804a1691aca2fffe4b633a33
jubeater <zhangtianlin15@gmail.com>
2019-03-11 02:55:16 -0500

    finish the base qs of section4

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,4 +1,4 @@
-var john = {
+/* var john = {
     name: 'john',
     yearOfBirth: 1990,
     job: 'teacher'
@@ -19,4 +19,41 @@ var john = new Person('john', 1990, 'teacher');
 
 john.calculateAge();
 
-var jane = new Person('Jane', 1969, 'engineer');
+var jane = new Person('Jane', 1969, 'engineer'); */
+(function() {
+    function Question(question, answers, correctAnswer){
+        this.question = question;
+        this.answers = answers;
+        this.correctAnswer = correctAnswer;
+    }
+    
+    function generationQuestions() {
+        var qone = new Question("what's my name?", ['jubeater','lasd','sadf'],0);
+        var qtwo = new Question('what\'s my birth year? ', [1993,1992,1994], 1);
+        var qthree = new Question('what\'s my age?', [14,15,27],2);
+        return [qone,qtwo,qthree];
+    }
+    
+    
+    function chooseQuestion(questions) {
+        return questions[Math.floor(Math.random()*3)];
+    }
+    
+    function displayQuestion(questions) {
+        for (let i  = 0; i < questions.answers.length; i++) {
+            console.log('' + i + ' ' + questions.answers[i]);
+        }
+        var input = prompt(questions.question);
+        checkCorrectness(questions,input);
+    }
+    
+    
+    function checkCorrectness(question, answer){
+        if (question.correctAnswer == answer) {
+            console.log("you are correct!");
+        } else {
+            console.log("you are wrong, the correct answer is the option " + question.correctAnswer);
+        }
+    }
+    displayQuestion(chooseQuestion(generationQuestions()));
+})();
