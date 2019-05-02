b8ab78c817b9b1b5cee878c63b183add44c74d97
jubeater <zhangtianlin15@gmail.com>
2019-05-02 16:16:25 -0500

    some progress

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -2,6 +2,7 @@ import Search from './models/Search.js';
 import Recipe from './models/Recipe.js';
 import {elements, renderLoader, clearLoader} from './views/base.js';
 import * as searchView from './views/searchView.js';
+import * as recipeView from './views/recipeView.js';
 /** Global state of the app
  *  - search object
  *  - cur recipe object
@@ -16,17 +17,23 @@ const controlSearch = async () => {
 
     if (query) {
         state.search = new Search(query);
+        try {
+            //prepare ui for results
+            searchView.clearInput();
+            searchView.clearResults();
+            //search for recipes
+            renderLoader(elements.searchRes);
+            await state.search.getResults();
+            clearLoader();
+            //render results on ui
+            console.log(state.search.result);
+            searchView.renderResults(state.search.result);
+        } catch(error) {
+            console.log("sth wrong with the searching...");
+            console.log(error);
 
-        //prepare ui for results
-        searchView.clearInput();
-        searchView.clearResults();
-        //search for recipes
-        renderLoader(elements.searchRes);
-        await state.search.getResults();
-        clearLoader();
-        //render results on ui
-        console.log(state.search.result);
-        searchView.renderResults(state.search.result);
+        }
+
 
     }
 
@@ -40,7 +47,7 @@ elements.searchForm.addEventListener('submit', e => {
 
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
-    console.log(btn);
+    //console.log(btn);
     if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
         searchView.clearResults();
@@ -48,6 +55,32 @@ elements.searchResPages.addEventListener('click', e => {
     }
 });
 
+const controlRecipe = async () => {
+    const id = window.location.hash.replace('#','');
+    //console.log(id);
+    if (id) {
+        //ui for changes
+        renderLoader(elements.recipe);
+        //create recipe object
+        state.recipe = new Recipe(id);
+        try {
+            //get recipe data
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
 
-const r = new Recipe(9630);
-r.getRecipe();
+            //calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+            //render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+        } catch(error) {
+            console.log('error processing recipe!');
+        }
+
+    }
+};
+
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+['hashchange','load'].forEach(event => window.addEventListener(event, controlRecipe));
--- 9-forkify/starter/src/js/models/Recipe.js
+++ 9-forkify/starter/src/js/models/Recipe.js
@@ -9,9 +9,75 @@ export default class Recipe{
         try {
             const res = await axios(`${proxy}https://www.food2fork.com/api/get?key=${key}&rId=${this.id}`);
             console.log(res);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
         } catch (error) {
             console.log(error);
         }
+    }
+
+    calcTime() {
+        const numIng = this.ingredients.length;
+        const periods = Math.ceil(numIng / 3);
+        this.time = periods * 15;
+    }
+
+    calcServings() {
+        this.servings = 4;
+    }
+
+    parseIngredients() {
+        const unitsLong = ['tablespoons', 'tablespoon','ounce','ounces','teaspoon','teaspoons','cups','pounds'];
+        const unitsShort = ['tbsp','tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const newIngredients = this.ingredients.map(el => {
+            //uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+            //remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, " ");
+            //parse ingredients into count, unit and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+
+            let objIng;
+            if (unitIndex > -1) {
+                //have unit
+                const arrCount = arrIng.slice(0, unitIndex);
+                let count;
+                if (arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-','+'));
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));
+                }
+
+                objIng = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' '),
+                }
+            } else if (parseInt(arrIng[0], 10)) {
+                //no unit but have number
+                objIng = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' '),
+                }
+            } else {
+                //no unit and number in 1st position
+                objIng = {
+                    count: 1,
+                    unit: '',
+                    ingredient,
+                }
+            }
+            return objIng;
+        });
+        this.ingredients = newIngredients;
     }
 
 }
